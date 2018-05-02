881e6bb176c0cde65b387563d931adb1f89e4773
Ivan Kryazhev <t9221823420@gmail.com>
2018-05-02 14:27:01 +0500

    daily

--- src/components/Migration.php
+++ src/components/Migration.php
@@ -422,6 +422,33 @@ abstract class Migration extends \yii\db\Migration
 		return $this->timestamp( null, true, true );
 	}
 	
+	/**
+	 * Creates a medium text column.
+	 * @return ColumnSchemaBuilder the column instance which can be further customized.
+	 */
+	public function mediumText()
+	{
+		return $this->getDb()->getSchema()->createColumnSchemaBuilder('mediumtext');
+	}
+	
+	/**
+	 * Creates a long text column.
+	 * @return ColumnSchemaBuilder the column instance which can be further customized.
+	 */
+	public function longText()
+	{
+		return $this->getDb()->getSchema()->createColumnSchemaBuilder('longtext');
+	}
+	
+	/**
+	 * Creates a tiny text column.
+	 * @return ColumnSchemaBuilder the column instance which can be further customized.
+	 */
+	public function tinyText()
+	{
+		return $this->getDb()->getSchema()->createColumnSchemaBuilder('tinytext');
+	}
+	
 	public function enum( $values = [] )
 	{
 		
